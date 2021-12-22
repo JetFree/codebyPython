d89d7ffca0661b16e8b04fc3ce908b0f52a7f310
Evgeny <jetfree@yandex.ru>
2021-12-22 08:13:09 +0300

    Corrected last requested remarks

--- lesson12/test6.py
+++ lesson12/test6.py
@@ -6,13 +6,11 @@ year_calendar = calendar.LocaleTextCalendar(locale="ru_RU.UTF-8")
 with open("calendar.txt", "w") as file:
     file.write(year_calendar.formatyear(2020, 0, 0, 6, 3))
 with open("calendar.txt", "r") as file:
-    file_lines = file.readlines()
-
-for line in file_lines:
-    if line.find("2020") > 0 or line.find("а") > 0:
-        print(Fore.GREEN + line + Fore.RESET, end="")
-    elif line.find("сб") > 0:
-        print(line.replace("сб вс", f"{Fore.RED}{'сб вс'}{Fore.RESET}"), end="")
-    else:
-        print(line, end="")
+    for line in file.readlines():
+        if "2020" in line or "а" in line:
+            print(f"{Fore.GREEN}{line}{Fore.RESET}", end="")
+        elif "сб" in line:
+            print(line.replace("сб вс", f"{Fore.RED}{'сб вс'}{Fore.RESET}"), end="")
+        else:
+            print(line, end="")
 
