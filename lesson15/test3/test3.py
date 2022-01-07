f5401291cfc93f049655098b142b1f61eaa66495
Evgeny <jetfree@yandex.ru>
2022-01-07 19:05:29 +0300

    Homeworks #3 and 5

--- lesson15/test3/test3.py
+++ lesson15/test3/test3.py
@@ -9,8 +9,8 @@ class LinesFinder:
         self.value = value
         self.base_file = "phonebook_al.txt"
         content = self.get_file_content()
-        content = self.find_lines(content)
-        self.print_result(content)
+        result = self.find_lines(content)
+        self.print_result(result)
 
     def get_file_content(self):
         with open(self.base_file, "r") as file:
@@ -19,17 +19,13 @@ class LinesFinder:
     def find_lines(self, lines):
         result_list = list()
         for line in lines:
-            if self.value.upper() in line or self.value.lower() in line:
+            if self.value.lower() in line.lower():
                 result_list.append(self.refactor_string(line))
         return result_list
 
     def refactor_string(self, string):
-        line_list = string.split()
-        if line_list[0].isupper():
-            line_list[0] = line_list[0].capitalize()
-        new_str = " ".join([line_part for line_part in line_list
-                            if "@" not in line_part])
-        return new_str
+        return " ".join([line_part for line_part in string.split()
+                         if "@" not in line_part]).title()
 
     def print_result(self, lines):
         print(f"Найдено {len(lines)} записей.")
---
+++ lesson15/test5/test5.py
@@ -0,0 +1,36 @@
+import click
+import itertools
+import datetime
+import random
+
+
+@click.command()
+@click.argument("length")
+class CombinationGenerator:
+
+    def __init__(self, length):
+        self.length = int(length)
+        comb_list = self.generate_combinations()
+        self.print_result(comb_list)
+        self.write_to_file(comb_list)
+
+    def generate_combinations(self):
+        int_list = random.sample("0123456789", self.length)
+        comb_list = list(itertools.product(int_list, repeat=4))
+        random.shuffle(comb_list)
+        return comb_list
+
+    def print_result(self, comb_list):
+        print(f"Start process...\nКоличество комбинаций: {len(comb_list)}")
+        [print("".join(comb)) for comb in comb_list]
+
+    def write_to_file(self, comb_list):
+        time_start = datetime.datetime.now()
+        with open(f"num_dict{self.length}.txt", "w") as file:
+            file.writelines([("".join(comb) + "\n") for comb in comb_list])
+        result = datetime.datetime.now().timestamp() - time_start.timestamp()
+        print(f"Completed in: {result:.2f} sec.")
+
+
+if __name__ == "__main__":
+    CombinationGenerator()
