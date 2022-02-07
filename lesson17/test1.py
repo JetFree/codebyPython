d936febd60b51e65ddc86682cbc036b60adf0fbc
JetFree <l7egnf@gmail.com>
2022-02-07 15:11:24 -0500

    Update test1.py

--- lesson17/test1.py
+++ lesson17/test1.py
@@ -1,34 +1,46 @@
 import mysql.connector
 
+val = [
+    ("батон нарезной", "21 руб"),
+    ("масло подсолнечное", "60 руб"),
+    ("крупа гречневая", "80 руб"),
+    ("молоко", "54 руб"),
+    ("яйцо куриное", "55 руб"),
+    ("кетчуп", "75 руб"),
+    ("сок томатный", "92 руб"),
+    ("макароны", "30 руб"),
+    ("зелёный корошек", "45 руб"),
+    ("селёдка", "150 руб"),
+]
+database_name = "first"
+db_connect = None
 
-if __name__ == "__main__":
-    mydb = mysql.connector.connect(
+
+def connect_database():
+    global db_connect
+    db_connect = mysql.connector.connect(
         database="mysql",
         host="localhost",
         user="jet_admin",
         passwd="123"
     )
-    database_name = "first"
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE DATABASE first")
-    mycursor.execute("USE first")
-    mycursor.execute("CREATE TABLE product (id INT AUTO_INCREMENT PRIMARY KEY,"
+
+
+def create_data():
+    mycursor = db_connect.cursor();
+    mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+    mycursor.execute(f"USE {database_name}")
+    mycursor.execute("CREATE TABLE IF NOT EXISTS product (id INT AUTO_INCREMENT PRIMARY KEY,"
                      "name VARCHAR(255),"
                      "price VARCHAR(255))")
     ins_tab = "INSERT INTO product (name, price) VALUES (%s, %s)"
-    val = [
-        ("батон нарезной", "21 руб"),
-        ("масло подсолнечное", "60 руб"),
-        ("крупа гречневая", "80 руб"),
-        ("молоко", "54 руб"),
-        ("яйцо куриное", "55 руб"),
-        ("кетчуп", "75 руб"),
-        ("сок томатный", "92 руб"),
-        ("макароны", "30 руб"),
-        ("зелёный корошек", "45 руб"),
-        ("селёдка", "150 руб"),
-    ]
+
     mycursor.executemany(ins_tab, val)
-    mydb.commit()
-    mydb.close()
+    db_connect.commit()
     print(f"The database '{database_name}' was created!")
+
+
+if __name__ == "__main__":
+    connect_database()
+    create_data()
+    db_connect.close()
