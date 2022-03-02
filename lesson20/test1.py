485c8ee0d2bd2edf73b6e9b1e04908fe9f3f647e
Evgeny <jetfree@yandex.ru>
2022-03-03 00:47:31 +0100

    added lesson20/test1

---
+++ lesson20/test1.py
@@ -0,0 +1,12 @@
+import threading
+import os
+
+
+def show_image(image_name):
+    os.system(f"open {image_name}")
+
+
+if __name__ == "__main__":
+    print("Привет Бро! Я занял первое место в конкурсе!")
+    t = threading.Timer(5, show_image, ["nb.png"])
+    t.start()
