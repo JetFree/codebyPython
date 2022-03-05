2f3a6adbcd504a6a308f6fa98739c27c8aa4be98
JetFree <l7egnf@gmail.com>
2022-03-05 16:49:53 -0500

    Update test3.py

--- lesson20/test3.py
+++ lesson20/test3.py
@@ -3,30 +3,23 @@ from colorama import Fore
 import time
 
 
-def start_race(car_name, race_time):
-    start_time = time.time()
-    print(f"Автомобиль {car_name} стартовал!")
-    time.sleep(race_time)
+def start_race(car_name, start_time):
     print(f"\nКоличество активных авто: {threading.active_count() - 1}")
     print(f"Автомобиль {Fore.GREEN + car_name + Fore.RESET} финишировал {find_place()}")
     print(Fore.LIGHTMAGENTA_EX + f"Доехал до финиша за: {time.time() - start_time:.3f} сек." + Fore.RESET)
 
 
 def find_place():
-    with lock:
-        number = 3
-        if threading.active_count() == 4:
-            number = 1
-        elif threading.active_count() == 3:
-            number = 2
+    number = 3
+    if threading.active_count() == 4:
+        number = 1
+    elif threading.active_count() == 3:
+        number = 2
     return number
 
 
 if __name__ == "__main__":
-    lock = threading.Lock()
-    cars = [threading.Thread(target=start_race, args=["MAZDA", 15]),
-            threading.Thread(target=start_race, args=["HONDA", 18]),
-            threading.Thread(target=start_race, args=["TOYOTA", 19])
-            ]
-    for t in cars:
-        t.start()
+    race_dict = {"MAZDA": 15, "HONDA": 18, "TOYOTA": 19}
+    for car_name, sec in race_dict.items():
+        print(f"Автомобиль {car_name} стартовал!")
+        threading.Timer(sec, start_race, [car_name, time.time()]).start()
