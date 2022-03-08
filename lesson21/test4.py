02da9ac6a74f4d1c24b3495edcf0515b944130ae
jetfree <l7egnf@gmail.com>
2022-03-08 06:57:02 -0500

    temporary version of task 4, multithreading

--- lesson21/test4.py
+++ lesson21/test4.py
@@ -6,22 +6,29 @@ class CartridgesBox:
     def __init__(self, amount):
         self.amount = amount
 
-    def get_cartridge(self):
-        self.amount -= 1
+    def get_cartridges(self):
+        with lock:
+            print(self.amount)
+            c_left = 30
+            if self.amount >= 30:
+                self.amount -= 30
+            else:
+                self.amount = 0
+                c_left = self.amount
+            return c_left
 
 
 def load_cage(number):
-    with lock:
-        print(f"Курсант {number} начал снаряжать магазин")
-        for i in range(1, 31):
-            general_box.get_cartridge()
-            print(i, end=" ")
-        print(f"\nКурсант {number} снарядил магазин\n")
+    print(f"Курсант {number} начал снаряжать магазин")
+    cartridges = general_box.get_cartridges()
+    for i in range(1, cartridges + 1):
+        print(i, end=" ")
+    print(f"\nКурсант {number} снарядил магазин\n")
 
 
 if __name__ == "__main__":
     lock = multiprocessing.Lock()
-    general_box = CartridgesBox(150)
+    general_box = CartridgesBox(140)
     processes = [multiprocessing.Process(target=load_cage, args=(n,)) for n in range(1, 6)]
     for p in processes:
         p.start()
