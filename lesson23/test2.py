add3fc1f4794b4b5049164491e5e966fc782506c
jetfree <l7egnf@gmail.com>
2022-03-15 18:30:52 -0400

    fixed concerns in task 2

--- lesson23/test2.py
+++ lesson23/test2.py
@@ -1,27 +1,24 @@
 import requests
 from colorama import Fore
 
-urls = [
-    "https://codeby.net/forums/",
-    "https://geekprank.com/hacker/typer/",
-    "https://bowandtie.ru/muzhskie-shlyapyi",
-    "https://xakep.ru/",
-    "https://sdelaicomp.ru/wi-fi/oshibka"
-]
-
 
 def format_response(status):
-    result = ""
     if status == 404:
-        result = Fore.RED + str(status) + Fore.RESET + " Not Found!"
+        return f"{Fore.RED}{status}{Fore.RESET} Not Found!"
     elif status == 301:
-        result = Fore.LIGHTBLUE_EX + str(status) + Fore.RESET + " Success!"
+        return f"{Fore.LIGHTBLUE_EX}{status}{Fore.RESET} Success!"
     else:
-        result = Fore.GREEN + str(status) + Fore.RESET + " Success!"
-    return result
+        return f"{Fore.GREEN}{status}{Fore.RESET} Success!"
 
 
 if __name__ == "__main__":
+    urls = [
+        "https://codeby.net/forums/",
+        "https://geekprank.com/hacker/typer/",
+        "https://bowandtie.ru/muzhskie-shlyapyi",
+        "https://xakep.ru/",
+        "https://sdelaicomp.ru/wi-fi/oshibka"
+    ]
     for url in urls:
         resp = requests.get(url, allow_redirects=False)
         print(f"{url} {format_response(resp.status_code)}")
