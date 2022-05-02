40b5040a854584ec5ad5262089339ca553c617a0
Jet <l7egnf@gmail.com>
2022-05-02 17:41:50 -0400

    fix problem with out of memory for more than 3 symbols length

--- lesson27/test1.py
+++ lesson27/test1.py
@@ -22,12 +22,13 @@ def calculate_combinations(length):
 
 def generate_comb_list(length):
     symbol_list = string.digits + string.ascii_lowercase + string.ascii_uppercase
-    return list(itertools.permutations(symbol_list, length))
+    return itertools.permutations(symbol_list, length)
 
 
-def write_to_file(perm_list):
+def write_to_file(perm_iter):
     with open("my_dict.txt", "w") as file:
-        file.writelines(["".join(perm) + "\n" for perm in perm_list])
+        for permutation in perm_iter:
+            file.write("".join(permutation) + "\n")
 
 
 if __name__ == "__main__":
