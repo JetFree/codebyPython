5571f0fef17e37a23cb0d2767740ca83e56028d6
Jet <l7egnf@gmail.com>
2022-05-23 14:09:36 -0400

    refactored first task about Cryptography

--- lesson31/test1.py
+++ lesson31/test1.py
@@ -5,29 +5,7 @@ def print_help():
     print("1 - encode text\n2 - decode text")
 
 
-def encode_text(text):
-    final = str()
-    for symbol in text:
-        for index_alpha, symbol_alpha in enumerate(c_alphabet_upper):
-            if symbol == symbol_alpha:
-                final += reversed_upper[index_alpha]
-    print("Encrypted message: " + final)
-
-
-def decode_text(text):
-    final = str()
-    for symbol in text:
-        for index_alpha_rev, symbol_alpha_rev in enumerate(reversed_upper):
-            if symbol == symbol_alpha_rev:
-                final += c_alphabet_upper[index_alpha_rev]
-    print("Decrypted message: " + final)
-
-
-if __name__ == '__main__':
-    c_alphabet_upper = [chr(x) for x in range(1040, 1072)]
-    reversed_upper = list(c_alphabet_upper)
-    reversed_upper.reverse()
-    print_help()
+def get_data():
     try:
         choice = int(input("Select action: "))
         text = input("Enter a message: ").upper()
@@ -35,8 +13,31 @@ if __name__ == '__main__':
         print("Incorrect value entered. Please select correct value")
         sys.exit(0)
     if choice == 1:
-        encode_text(text)
+        return text, True
     elif choice == 2:
-        decode_text(text)
+        return text, False
     else:
         print("Incorrect value entered. Please select correct value")
+
+
+def encode_text(text, is_encode):
+    final = str()
+    alpha = reversed_upper
+    revers_alpha = c_alphabet_upper
+    if is_encode:
+        alpha = c_alphabet_upper
+        revers_alpha = reversed_upper
+    for symbol in text:
+        for index_alpha, symbol_alpha in enumerate(alpha):
+            if symbol == symbol_alpha:
+                final += revers_alpha[index_alpha]
+    print("Encrypted message: " + final)
+
+
+if __name__ == '__main__':
+    c_alphabet_upper = [chr(x) for x in range(1040, 1072)]
+    reversed_upper = list(c_alphabet_upper)
+    reversed_upper.reverse()
+    print_help()
+    text, choice = get_data()
+    encode_text(text, choice)
